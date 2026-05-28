0ac423c8518042942336d0af040d5d3f564c30f5
ZX <sam.lin.0202www@gmail.com>
2026-05-28 09:35:53 +0800

    CAMERA_TEST第一次修正

--- camera_test.py
+++ camera_test.py
@@ -1,11 +1,33 @@
 from ultralytics import YOLO
+import cv2
 
-# 1. 載入你訓練好的模型
 model = YOLO(r"runs\detect\train4\weights\best.pt")
 
-# 2. 開啟攝影機即時辨識
-model.predict(
-    source=0,      # 0 = webcam
-    show=True,     # 顯示畫面
-    conf=0.5       # 信心門檻（可調）
-)
+cap = cv2.VideoCapture(0)
+
+TARGETS = ["bottle", "can"]
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame, conf=0.5)
+
+    annotated_frame = results[0].plot()
+
+    for r in results:
+        for box in r.boxes:
+            cls_id = int(box.cls[0])
+            name = model.names[cls_id]
+
+            if name in TARGETS:
+                print("🚨 Detected:", name)
+
+    cv2.imshow("YOLO", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
